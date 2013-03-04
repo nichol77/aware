7a62fdf2522b081305303c63a7340c419a8e2a14
Ryan J. Nichol <r.nichol@ucl.ac.uk>
2013-03-04 17:51:48 +0000

    Committing changes made at work today

--- browseEvents.php
+++
@@ -1,6 +0,0 @@
-<?php
-require("utils.php");
-$contentPage=basename($_SERVER['PHP_SELF'], ".php");
-doPage("content/$contentPage.php","Browse Events","Browse Events");
-//doPage("content/browseEvents.php");
-?>
