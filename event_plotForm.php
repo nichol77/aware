d7da594322279959ab843fe9745e9e1a34ab8176
Ryan Nichol <r.nichol@ucl.ac.uk>
2013-03-07 19:35:19 +0000

    Adding event selection box

--- event_plotForm.php
+++ event_plotForm.php
@@ -1,11 +1,11 @@
 <?php
 
 
-$run=$_GET["run"];
-if($_GET["run"] === null) {
-  $run=1999;
+$event=$_GET["event"];
+if($_GET["event"] === null) {
+  $event=1999;
  }
-echo "<form name=\"runForm\"   id=\"runForm\" action=\"javascript:drawPlot(); javascript:void(0);\">";		
+echo "<form name=\"eventForm\"   id=\"eventForm\" action=\"javascript:drawPlot(); javascript:void(0);\">";		
 
 echo "Event: <input type=\"text\" name=\"eventInput\" id=\"eventInput\" value=\"$event\"  />";
 echo '<br />';
