dc381a8ed73c456889991fa3bdf9c7160ab8bedb
Ryan Nichol <r.nichol@ucl.ac.uk>
2013-12-08 22:16:46 +0000

    Debugging event location finder

--- web/src/awareEvent.js
+++ web/src/awareEvent.js
@@ -1106,8 +1106,8 @@ function findBestLocation()
 	    refIndex=new Number(index);
 	}
     }
-    $("#debugContainer").append("<p>"+refIndex+" "+minDelta+"</p>");
-    var refLocation=AwareEvent.instrumentGeom.antList[AwareEvent.inputChanList[refIndex]].location;
+    $("#debugContainer").append("<p>"+refIndex+" "+minDelta+" "+AwareEvent.inputChanList[refIndex]+"</p>");
+    var refLocation=AwareEvent.instrumentGeom.antList[0].location;
     $("#debugContainer").append("<p>"+refLocation+" "+AwareEvent.instrumentGeom.antList[AwareEvent.inputChanList[refIndex]].location[1]+" "+refLocation[2]-+"</p>");
     //refIndex defines the antenna which all deltas are measured against    
     for(var index=0;index<AwareEvent.csumDeltaTArray.length;index++) {
