8cb048bc50f73645ab8c1248b5f3781e421e1416
Ryan Nichol <r.nichol@ucl.ac.uk>
2013-12-08 22:09:24 +0000

    Debugging event location finder

--- web/src/awareEvent.js
+++ web/src/awareEvent.js
@@ -1111,7 +1111,7 @@ function findBestLocation()
     for(var index=0;index<AwareEvent.csumDeltaTArray.length;index++) {
 	var rawLocation=AwareEvent.instrumentGeom.antList[AwareEvent.inputChanList[index]].location;
 	relLocationArray.push([rawLocation[0]-refLocation[0],rawLocation[1]-refLocation[1],rawLocation[2]-refLocation[2]]);
-	$("#debugContainer").append("<p>"+rawLocation[0]+" "+rawLocation[1]+" "+rawLocation[2]+"</p>");
+	$("#debugContainer").append("<p>"+rawLocation[0]-refLocation[0]+" "+rawLocation[1]-refLocation[1]+" "+rawLocation[2]-refLocation[2]+"</p>");
     }
 
 }
