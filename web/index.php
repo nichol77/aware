a330876207cc5a17caab5a371a8b7b5ddafdf4d6
Ryan J. Nichol <r.nichol@ucl.ac.uk>
2014-07-02 21:29:07 +0100

    Adding commits, trying to get to a place where the ARA and ANITA styles play nice with each other

--- web/index.php
+++ web/index.php
@@ -7,8 +7,8 @@ header("Connection: keep-alive");
 <head>
 <link rel="StyleSheet" href="styles/base.css" type="text/css" media="screen" >
 <link rel="StyleSheet" href="styles/aware.css" type="text/css" media="screen" />
-<link rel="StyleSheet" href="styles/local.css" type="text/css" media="screen" />
-<title>ANITA-2 - AWARE</title><META http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+<link rel="StyleSheet" href="styles/ara.css" type="text/css" media="screen" />
+<title>ARA - AWARE</title><META http-equiv="Content-Type" content="text/html; charset=utf-8"> 
 <script language="javascript" type="text/javascript" src="src/flot/jquery.min.js.gz"></script>
 </head>
 
@@ -18,17 +18,17 @@ header("Connection: keep-alive");
 
 
 <DIV class="heading">
-<h1>ANITA Web Monitor</h1>
+<h1>ARA Web Monitor</h1>
 </DIV>
 <DIV class=single>
 <DIV class=content>
-<h2>ANITA-2</h2>
-<a href="events.php?instrument=ANITA2">
+<h2>ARA02</h2>
+<a href="events.php?instrument=STATION2">
 <div class="lastBox">
   Last Event: 
 <span id="lastEvent">
 <?php
-$lastEvent='output/ANITA2/lastEvent';
+$lastEvent='output/STATION2/lastEvent';
 include $lastEvent ;
 echo "</span>";
 if (file_exists($lastEvent)) {
@@ -38,28 +38,41 @@ if (file_exists($lastEvent)) {
 </div>
 </a>
 
-
-<a href="awareHk.php?instrument=ANITA2&hkType=hk">
+<a href="awareHk.php?instrument=STATION2&hkType=eventHk">
 <div class="lastBox">
-  Last Hk: 
-<span id="lastHk">
+  Last Event Hk: 
+<span id="lastEventHk">
 <?php
-$lastHk='output/ANITA2/lastHk';
-include $lastHk ;
+$lastEventHk='output/STATION2/lastEventHk';
+include $lastEventHk ;
 echo "</span>";
-if (file_exists($lastHk)) {
-    echo " started " . date ("F d Y H:i.", filemtime($lastHk));
+if (file_exists($lastEventHk)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastEventHk));
+}
+?>
+</div>
+</a>
+<a href="awareHk.php?instrument=STATION2&hkType=sensorHk">
+<div class="lastBox">
+  Last Sensor Hk: 
+<span id="lastSensorHk">
+<?php
+$lastSensorHk='output/STATION2/lastSensorHk';
+include $lastSensorHk ;
+echo "</span>";
+if (file_exists($lastSensorHk)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastSensorHk));
 }
 ?>
 </div>
 </a>
 
-<a href="awareHk.php?instrument=ANITA2&hkType=header">
+<a href="awareHk.php?instrument=STATION2&hkType=header">
 <div class="lastBox">
   Last Header: 
 <span id="lastHeader">
 <?php
-$lastHeader='output/ANITA2/lastHeader';
+$lastHeader='output/STATION2/lastHeader';
 include $lastHeader ;
 echo "</span>";
 if (file_exists($lastHeader)) {
@@ -69,6 +82,128 @@ if (file_exists($lastHeader)) {
 </div>
 </a>
 
+
+<h2>ARA03</h2>
+<a href="events.php?instrument=STATION3">
+<div class="lastBox">
+  Last Event:
+<span id="lastEvent">
+<?php
+$lastEvent='output/STATION3/lastEvent';
+include $lastEvent ;
+echo "</span>";
+if (file_exists($lastEvent)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastEvent));
+}
+?>
+
+</div>
+</a>
+<a href="awareHk.php?instrument=STATION3&hkType=eventHk">
+<div class="lastBox">
+  Last Event Hk:
+<span id="lastEventHk">
+<?php
+$lastEventHk='output/STATION3/lastEventHk';
+include $lastEventHk ;
+echo "</span>";
+if (file_exists($lastEventHk)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastEventHk));
+}
+?>
+</div>
+</a>
+<a href="awareHk.php?instrument=STATION3&hkType=sensorHk">
+<div class="lastBox">
+  Last Sensor Hk:
+<span id="lastSensorHk">
+<?php
+$lastSensorHk='output/STATION3/lastSensorHk';
+include $lastSensorHk ;
+echo "</span>";
+if (file_exists($lastSensorHk)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastSensorHk));
+}
+?>
+</div>
+</a>
+
+<a href="awareHk.php?instrument=STATION3&hkType=header">
+<div class="lastBox">
+  Last Header: 
+<span id="lastHeader">
+<?php
+$lastHeader='output/STATION3/lastHeader';
+include $lastHeader ;
+echo "</span>";
+if (file_exists($lastHeader)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastHeader));
+}
+?>
+</div>
+</a>
+
+
+
+<h2>ARA01</h2>
+<a href="events.php?instrument=STATION1B">
+<div class="lastBox">
+  Last Event:
+<span id="lastEvent">
+<?php
+$lastEvent='output/STATION1B/lastEvent';
+include $lastEvent ;
+echo "</span>";
+if (file_exists($lastEvent)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastEvent));
+}
+?>
+
+</div>
+</a>
+<a href="awareHk.php?instrument=STATION1B&hkType=eventHk">
+<div class="lastBox">
+  Last Event Hk:
+<span id="lastEventHk">
+<?php
+$lastEventHk='output/STATION1B/lastEventHk';
+include $lastEventHk ;
+echo "</span>";
+if (file_exists($lastEventHk)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastEventHk));
+}
+?>
+</div>
+</a>
+<a href="awareHk.php?instrument=STATION1B&hkType=sensorHk">
+<div class="lastBox">
+  Last Sensor Hk:
+<span id="lastSensorHk">
+<?php
+$lastSensorHk='output/STATION1B/lastSensorHk';
+include $lastSensorHk ;
+echo "</span>";
+if (file_exists($lastSensorHk)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastSensorHk));
+}
+?>
+</div>
+</a>
+
+<a href="awareHk.php?instrument=STATION1B&hkType=header">
+<div class="lastBox">
+  Last Header: 
+<span id="lastHeader">
+<?php
+$lastHeader='output/STATION1B/lastHeader';
+include $lastHeader ;
+echo "</span>";
+if (file_exists($lastHeader)) {
+    echo " started " . date ("F d Y H:i.", filemtime($lastHeader));
+}
+?>
+</div>
+</a>
 
 </div>
 </div>
