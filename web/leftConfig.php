dea0bb28de8a3e324b7b80e619d15dc5d033eca5
Ryan J. Nichol <r.nichol@ucl.ac.uk>
2014-09-05 12:36:25 +0100

    Starting to get a config view going

---
+++ web/leftConfig.php
@@ -0,0 +1,59 @@
+
+<div class="formDiv">
+<?php
+  //First up include the really static links
+include("leftMain.php");
+?>
+
+
+<form class="runForm" id="runForm" action=" javascript:void(0);">  
+<?php
+ //Load the defaults
+$defaults_array = parse_ini_file("config/defaultValues.ini", true);
+$realDefaults = array();
+foreach($defaults_array as $ignore => $properties){
+  $realDefaults["instrument"]=$properties[instrument];
+  $realDefaults["hkType"]=$properties[hkType];
+}
+
+$instrument=$_GET["instrument"];
+if($_GET["instrument"] === null) {
+  $instrument=$realDefaults[instrument];
+ }
+
+$run=$_GET["run"];
+if($_GET["run"] === null) {
+  $run=151; //This number is never really used
+ }
+
+
+
+echo "<fieldset>";     
+echo "<legend>Update Plot</legend>";
+///Load the instrument array config file
+$inst_array = parse_ini_file("config/instrumentList.ini", true);
+echo '<label>Instrument:</label> <select id="instrumentForm" >';
+foreach($inst_array as $inst => $properties){
+  $key=$properties[name];
+  $value=$properties[title];
+  $pos = strpos($instrument,$key);
+  if($pos !== false) {
+  echo "<option value=$key selected=\"selected\" label=\"$value\">$value</option>";
+  }
+  else {
+    echo "<option value=$key label=\"$value\">$value</option>";    
+  }         
+}
+echo "</select>";
+echo "<br>";
+
+echo "<label>Run:</label>";
+echo "<input type=\"number\" name=\"runInput\" id=\"runInput\" value=\"$run\" max=\"100000\" min=\"0\"  >";
+?>
+</fieldset>
+</form>
+</div>
+
+
+
+ 
---
+++ web/runConfig.php
@@ -0,0 +1,66 @@
+<? 
+ob_start("ob_gzhandler"); 
+header("Connection: keep-alive");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="StyleSheet" href="styles/jquery-ui-1.10.3.custom.min.css" type="text/css" media="screen" >
+<link rel="StyleSheet" href="styles/aware.css" type="text/css" media="screen">
+<link rel="StyleSheet" href="styles/base.css" type="text/css" media="screen">
+<link rel="StyleSheet" href="styles/help.css" type="text/css" media="screen" >
+<link rel="StyleSheet" href="styles/local.css" type="text/css" media="screen" >
+
+<title>AWARE Config View</title><META http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+<script type="text/javascript" src="src/awareUtils.js"></script>
+<script type="text/javascript" src="src/awareHk.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script type="text/javascript" src="src/flot/jquery.flot.min.js.gz"></script>
+<script type="text/javascript" src="src/flot/jquery.flot.errorbars.min.js.gz"></script>
+<script type="text/javascript" src="src/flot/jquery.flot.time.min.js.gz"></script>
+<script type="text/javascript" src="src/flot/jquery.flot.canvas.min.js.gz"></script>
+<script type="text/javascript" src="src/flot/jquery.flot.selection.min.js.gz"></script>
+<script type="text/javascript" src="src/flot/jquery.flot.resize.min.js.gz"></script>
+<script type="text/javascript">
+
+  $(function() {
+      initialiseConfigView();
+  });  
+
+</script>
+</head>
+
+
+
+<body>
+
+
+<div class="heading">
+<div id="titleContainer">
+<h1>AWARE Config View</h1>
+</div>
+</div>
+<div class=middle>
+
+<div id="configView" class="configView" ></div>
+
+<div id="plot-holder-1" class="plot-holder"  >
+<div id="plot-header-1" class="plot-header">
+<h3>Config View</h3>
+</div>
+<div id="plot-content-1" class="plot-content" style="width:100%; height:96% ; "></div>
+</div>
+<div id="debugContainer"></div>
+
+</div> 
+
+<div class="leftBar" id="leftbar">
+<?php
+include("leftConfig.php");
+?>
+</div>
+
+
+</body></html>
+
