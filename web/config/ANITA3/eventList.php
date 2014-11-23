a67c6e6101ffadbdab5662fafba9fbebfabce60a
radio <radio@anitarf.ldb>
2014-11-23 22:06:33 +1300

    Moving things around

---
+++ web/config/ANITA3/eventList.php
@@ -0,0 +1,19 @@
+<?php
+class mFile
+{
+    public $name, $time, $size;
+}
+
+$run=$_GET["run"];
+
+
+foreach (glob("output/ANITA3//*.json") as $curFilename)
+{
+    $curFileObj = new mFile;
+    $curFileObj->name = $curFilename;
+    $curFileObj->time = date("d/m/Y - H:i", filectime($curFilename));
+    $curFileObj->size = filesize($curFilename);
+    $fileArray[] = $curFileObj;
+}
+printf("%s", json_encode($fileArray));
+?> 
