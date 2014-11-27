7b7af0658603a3c7ac63567ba1b93ee5b322f805
Chris <Chrispdavenport@googlemail.com>
2014-11-27 14:24:51 +0000

    Added pngCrawler which returns json of the pngs
    for a given run or all runs.

--- web/config/ANITA3/newFileChecker.php
+++ web/config/ANITA3/newFileChecker.php
@@ -1,8 +1,18 @@
 <?php
+	//
+	// php file which returns a simple json string of the modified time of the 
+	// housekeeping and event files from the hkTypeList.ini
+	//
+
+	//Get a list of types
 	$hkType_array = parse_ini_file("config/ANITA3/hkTypeList.ini", true);
+	// Atempts lastevent check also, suppress a warning if failed
 	@$lastEvent = filemtime('output/ANITA3/lastEvent');
+	
+	//Start json string
 	echo "{\"Event\":\"$lastEvent\"";
 	
+	//For each item in the hklist add a new item to the json string
 	foreach($hkType_array as $inst => $properties){
 		$key = ucfirst($properties['name']);
 		$lastHk="output/ANITA3/last$key";
@@ -11,5 +21,6 @@
 			echo ",\"$key\":\"$time\"";
 		}
 	}
+	//Finish the json string.
 	echo "}";
 ?>
