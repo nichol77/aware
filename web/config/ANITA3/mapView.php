c4b61a0b831153547bff302953e6553bf2c54093
Ryan Nichol <r.nichol@ucl.ac.uk>
2014-12-30 16:42:50 +0000

    Trying to add extra functionality to the map view

--- web/config/ANITA3/mapView.php
+++ web/config/ANITA3/mapView.php
@@ -55,10 +55,10 @@ header("Connection: keep-alive");
 </div>
 
 <div id="plot-content-1" class="plot-content" style="width:100%; height:96% ; ">
+
 <div id="divMap-1" style="width:80%; height80%; padding: 0px; float : left; "></div>
 
-<div id="divMapInfo" style="width:80%; height10%; padding: 0px; float : left;"></div>
-
+<div id="divMapInfo" ></div>
 <div id="debugContainer"></div>
 </div>
 </div>
