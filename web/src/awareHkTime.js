2dcc1427ecbe0065b43dd63d23c2d940aeac917a
Ryan Nichol <r.nichol@ucl.ac.uk>
2015-07-23 16:37:38 -0500

    Trying to debug highcharts

--- web/src/awareHkTime.js
+++ web/src/awareHkTime.js
@@ -757,13 +757,12 @@ function actuallyDrawTheStuff(awareControl) {
     function doSomeTimePlotZoomingYaxis(event) {
 	//Need to zoom in on the proj plot
 	$('#debugContainer').append("<p>doSomeTimePlotZoomingYaxis:"+event.min+" "+event.max+" "+event.dataMax+" "+event.dataMin+"</p>");
-	if(lastMin!=event.min || lastMax!=event.max) {
-	    
-	    if(event.min!=null)
-		projPlot.xAxis[0].setExtremes(event.min,event.max);
-	    else
-		projPlot.xAxis[0].setExtremes(undefined,undefined);
-	}
+//	if(lastMin!=event.min || lastMax!=event.max) {	    
+//	    if(event.min!=null)
+//		projPlot.xAxis[0].setExtremes(event.min,event.max);
+//	    else
+//		projPlot.xAxis[0].setExtremes(undefined,undefined);
+//	}
 	lastMin=eventMin;
 	lastMax=eventMax;
     }
